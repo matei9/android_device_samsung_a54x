98202c308f06b27946aa32b24c11c28773e9c7e2
Tim Zimmermann <tim@linux4.de>
2023-03-08 10:19:50 +0100

    f62: Initial proprietary-files list
    Change-Id: I24333123267c9c7c04aae348bc0aa1d6db4ffb8d

--- extract-files.sh
+++ extract-files.sh
@@ -11,18 +11,14 @@ function blob_fixup() {
         vendor/lib*/libsec-ril*.so)
             "${PATCHELF}" --replace-needed libril.so libril-samsung.so "${2}"
             ;;
-        vendor/lib/hw/audio.primary.exynos9820.so)
+        vendor/lib/hw/audio.primary.exynos9825.so)
             "${PATCHELF}" --remove-needed libaudio_soundtrigger.so "${2}"
             "${PATCHELF}" --add-needed libshim_audioparams.so "${2}"
             sed -i 's/str_parms_get_str/str_parms_get_mod/g' "${2}"
             ;;
         vendor/lib64/libexynoscamera3.so)
-            xxd -p "${2}" | sed "s/8b022036/1f2003d5/g" | xxd -r -p > "${2}".patched
+            xxd -p "${2}" | sed "s/cc022036/1f2003d5/g" | xxd -r -p > "${2}".patched
             mv "${2}".patched "${2}"
-            ;;
-        vendor/lib*/sensors.*.so)
-            "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
-            sed -i 's/_ZN7android6Thread3runEPKcim/_ZN7utils326Thread3runEPKcim/g' "${2}"
             ;;
     esac
 }
