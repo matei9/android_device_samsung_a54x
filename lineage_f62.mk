1707878b0149b8890faf5dc5743716b2429c8009
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    f62: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- lineage_f62.mk
+++ lineage_f62.mk
@@ -30,9 +30,6 @@ TARGET_SCREEN_WIDTH := 1080
 ## Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-## Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_f62
 PRODUCT_DEVICE := f62
